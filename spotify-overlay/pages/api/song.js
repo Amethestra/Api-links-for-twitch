5248bd8e1b2175816d43b05e6998389efcde368e
Amethestra <Amethestra@gmail.com>
2025-06-12 18:36:38 +0100

    Confusion on initial build, file types have been corrected

--- spotify-overlay/pages/api/song.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    isPlaying: true,
-    name: "Test Song",
-    artist: "Test Artist",
-    albumArt: "https://via.placeholder.com/64"
-  });
-}
